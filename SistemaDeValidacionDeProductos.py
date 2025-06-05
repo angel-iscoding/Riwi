cf63ad6e1b2689a5aefe6bc1ef82deadbb4473dd
angel-iscoding <angel.iscoding@gmail.com>
2025-06-05 08:49:28 -0500

    Solving errors

--- SistemaDeValidacionDeProductos.py
+++ SistemaDeValidacionDeProductos.py
@@ -33,87 +33,100 @@ Seleccione una opcion: """
 exit_aplication = 2
 
 while exit_aplication == 2:
-    
-    print("¡Bienvenido a su Ecommerce favorito!")
+    try:
+        print("\n¡Bienvenido a su Ecommerce favorito!")
 
-    option = 0
-    
-    option = int(input(menu))
-
-    if (option == 1):
-        print("\nMostrando productos en el carrito\n")
-
-        total = 0 
-        discount = 0
-
-        for index, product in enumerate(cart): 
-            print(f"Producto #{index + 1}.")
-            print(f"Nombre: {product['name']}")
-            print(f"Precio: {product['price']}")
-            print(f"Cantidad: {product['stock']}")
-            print(f"Descuento: {product['discount']}\n")
-
-            discount = product["price"] * product["discount"]/100
-            total += product["price"] - discount
-
-        print(f"Total carrito: {total}")
-
-    elif (option == 2):
-        print("\nAgregar nuevo producto.\n")
-
-        name = input("Nombre del producto: ")
-        price = input("Precio del producto: ")
-        stock = input("Cantidad de products: ")
-        discount = input("Descuento (Solo numero): ")
-
-        new_product = {
-            "name": name,
-            "price": price,
-            "stock": stock,
-            "discount": discount
-        }
-
-        cart.append(new_product)
-
-        print("¡Producto agregado con exito!: " + str(new_product))
-
-    elif (option == 3):
-        print("\nEliminar producto.\n")
-
-        exit = 2
-
-        while exit == 2:
-
-            search = input("Nombre del producto: ")
-
-            for product in (cart):
-                if (search == product["name"]):
-                    cart.remove(product)
-
-            exit = int(input(
-                "Producto eliminado con exito. ¿Desea salir? (1) Si / (2) No. "))
-    elif (option == 4): 
-        print("\nComprar carrito.\n")
-
-        total = 0 
-        discount = 0
-
-        for product in cart:
-            discount = price * product["discount"]/100
-            total += product["price"] - discount
-
-        confirm = int(input(f"¿Estas seguro de hacer esta compra por el total de ${total}? (1) Si / (2) No.\n"))
-
-        if (confirm == 1):
-            print("¡Compra realizada con exito!")
-        elif (confirm == 2):
-            continue
-        else:
-            print("Opcion no valida.")
-            continue
-
-    else:
-        exit_aplication = int(input("Opcion no valida. ¿Desea salir? (1) Si / (2) No. "))
-    
-
+        option = 0
         
+        option = int(input(menu))
+
+        if (option == 1):
+            print("\nMostrando productos en el carrito\n")
+
+            total = 0 
+            discount = 0
+
+            for index, product in enumerate(cart): 
+                print(f"Producto #{index + 1}.")
+                print(f"Nombre: {product['name']}")
+                print(f"Precio: {product['price']}")
+                print(f"Cantidad: {product['stock']}")
+                print(f"Descuento: {product['discount']}\n")
+
+                discount = product["price"] * product["discount"]/100
+                total += product["price"] - discount
+
+            print(f"Total carrito: {total}")
+
+        elif (option == 2):
+            print("\nAgregar nuevo producto.\n")
+
+            name = input("Nombre del producto: ")
+            price = input("Precio del producto: ")
+            stock = input("Cantidad de products: ")
+            discount = input("Descuento (Solo numero): ")
+
+            if discount > 100 or discount < 0:
+                print("El descuento no puede ser mayor a 100% ni menor a 0%")
+                discount = 0
+            
+            new_product = {
+                "name": name,
+                "price": price,
+                "stock": stock,
+                "discount": discount
+            }
+
+            cart.append(new_product)
+
+            print("¡Producto agregado con exito!: " + str(new_product))
+
+        elif (option == 3):
+            print("\nEliminar producto.\n")
+
+            exit = 2
+
+            while exit == 2:
+
+                search = input("Nombre del producto: ")
+
+                for product in (cart):
+                    if (search == product["name"]):
+                        cart.remove(product)
+
+                exit = int(input(
+                    "Producto eliminado con exito. ¿Desea salir? (1) Si / (2) No. "))
+                
+        elif (option == 4): 
+            print("\nComprar carrito.\n")
+
+            total = 0 
+            discount = 0
+
+            for product in cart:
+                discount = price * product["discount"]/100
+                total += product["price"] - discount
+
+            confirm = int(input(f"¿Estas seguro de hacer esta compra por el total de ${total}? (1) Si / (2) No.\n"))
+
+            if (confirm == 1):
+                print("¡Compra realizada con exito!")
+            elif (confirm == 2):
+                continue
+            else:
+                print("Opcion no valida.")
+                continue
+
+        else:
+            exit_aplication = int(input("Opcion no valida. ¿Desea salir? (1) Si / (2) No. "))
+    except:
+        try:
+            exit_aplication = int(input("Ha ocurrido un error durante la ejecucion.\n¿Desea salir de aplicacion? (1) Si / (2) No. "))
+            if exit_aplication != 2 and exit_aplication != 1:
+                raise ValueError("Valor no valido.")
+            
+        except:
+            print("Respuesta no valida. Cerrando la aplicacion...")
+            exit_aplication = 1
+            
+    
